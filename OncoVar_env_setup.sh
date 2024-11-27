e406271a0cde84ad9af14c05e6e3ba63001aafab
Walter Odur <138713366+Walter-Odur@users.noreply.github.com>
2024-11-27 11:09:57 +0300

    Add files via upload

--- OncoVar_env_setup.sh
+++ OncoVar_env_setup.sh
@@ -1,44 +1,66 @@
-# Please read this carefully and ensure your environment is fully set then proceed to the submission script. 
+#!/bin/bash
 
-# Install conda/mamba if not yet installed
-wget https://github.com/conda-\
-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh
+: <<'COMMENT'
+Install conda/mamba if not yet installed following this instruction.
+
+wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh
 
 bash Miniforge3-Linux-x86_64.sh # this will create a directory miniforge3
 
+COMMENT
+
+
 # create a directory to store your executable files
 mkdir -p ~/tools/bin
-cp -r miniforge3/bin/* ~/tools/bin
-export PATH=$PATH:~/tools/bin
-
 
 # create & activate conda environment for the analysis
 # You can chose to use conda or mamba.
-# with conda
-conda create -n gatk
-conda activate gatk
 
-# OR
-mamba create -n gatk
-mamba activate gatk
+# Check if mamba is installed
+if command -v mamba &> /dev/null;
+then
+        # create conda environment and install necessary tools including java version 11 or higher
+        echo "Installing tools using mamba"
+        mamba create \
+                -n gatk \
+                -y \
+                -c bioconda \
+                -c conda-forge \
+                openjdk \
+                python=3.12 \
+                fastqc=0.11 \
+                fastp=0.23 \
+                bwa=0.7 \
+                samtools=1.21 \
+                bcftools=1.21 
+                
+        mamba activate gatk        
 
-# Install java version 11 or higher in the gatk environment
-conda install -c conda-forge openjdk
-# OR
-mamba install -c conda-forge openjdk
-java -version  # check the version of installed java
+elif command -v conda &> /dev/null;
+then
+        echo "Installing tools using conda"
+        conda create \
+                -n gatk \
+                -y \
+                -c bioconda \
+                -c conda-forge \
+                openjdk \
+                python=3.12 \
+                fastqc=0.11 \
+                fastp=0.23 \
+                bwa=0.7 \
+                samtools=1.21 \
+                bcftools=1.21 
+                
+        conda activate gatk        
+else
+        echo "OOPs..! miniconda/anaconda/micromamba not installed. Please install miniconda using the following commands and try again!"
+        echo '
+wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh && bash Miniforge3-Linux-x86_64.sh'
+        
+        exit 1
+fi
 
-# Run this command to install the following tools
-conda install -y \
-       -c bioconda \
-       -c conda-forge \
-       fastqc=0.11 fastp=0.23 bwa=0.7 samtools=1.21 bcftools=1.21 
-
-# OR
-mamba install -y \
-       -c bioconda \
-       -c conda-forge \
-       fastqc=0.11 fastp=0.23 bwa=0.7 samtools=1.21 bcftools=1.21 
 
 # Download and unzip gatk-4.2.2.0
 wget https://github.com/broadinstitute/gatk/releases/download/4.2.2.0/gatk-4.2.2.0.zip
